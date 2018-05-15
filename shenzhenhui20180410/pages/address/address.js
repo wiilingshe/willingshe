b0ae5a6e6079d5697f3999a3a24f93cf28a07393
willingshe <1214220846@qq.com>
2018-05-15 21:56:00 +0800

    11

--- shenzhenhui20180410/pages/address/address.js
+++ shenzhenhui20180410/pages/address/address.js
@@ -18,9 +18,9 @@ Page({
     array2:[],
     array3:[],
     region: ['广东省', '广州市', '海珠区'] ,
-    province1:[{name:'广东省',code:'1'},{name:'广东省',code:'2'},{name:'广东省',code:'3'},{name:'广东省',code:'4'},{name:'广东省',code:'5'},{name:'广东省',code:'6'},{name:'广东省',code:'7'},{name:'广东省',code:'8'},{name:'广东省',code:'9'},{name:'广东省',code:'10'}],
-    city1:[{name:'广州市','code':1},{name:'广州市','code':2},{name:'广州市','code':3},{name:'广州市','code':4},{name:'广州市','code':5},{name:'广州市','code':6},{name:'广州市','code':7},{name:'广州市','code':8}],
-    county1:[{name:'天河区',code:1},{name:'天河区',code:2},{name:'天河区',code:3},{name:'天河区',code:4},{name:'天河区',code:5},{name:'天河区',code:6},{name:'天河区',code:7},{name:'天河区',code:8},{name:'天河区',code:9},{name:'天河区',code:10}],
+    province1:[],
+    city1:[],
+    county1:[],
     provincecode:'',
     citycode:'',
     areacode:'',
@@ -147,17 +147,22 @@ Page({
       ifselect:true,
       array:that.data.province1
     })
-    // wx.request({
-    //   url:app.globalData.url+'/api/member/address_query',
-    //   method:'POST',
-    //   data:{access_token: app.accessToken(),type:0},
-    //   success:function(res){
-    //     console.log(res.data.data)
-    //     that.setData({
-    //       array:res.data.data
-    //     })
-    //   }
-    // })
+    wx.getStorage({
+      key:'access_token',
+      success:function(res1){
+        wx.request({
+          url:app.data.url+'/api/address/address_query',
+          method:'POST',
+          data:{access_token: res1.data,type:0},
+          success:function(res){
+            console.log(res.data.data)
+            that.setData({
+              array:res.data.data
+            })
+          }
+        })
+      }
+    })
   },
   getshi(e){
     var that = this;
@@ -171,17 +176,22 @@ Page({
       area:'',
       array2:that.data.city1
     })
-    // wx.request({
-    //   url:app.globalData.url+'/api/member/address_query',
-    //   method:'POST',
-    //   data:{access_token: app.accessToken(),type:1,provincecode:e.currentTarget.id},
-    //   success:function(res){
-    //     console.log(res.data.data)
-    //     that.setData({
-    //       array2:res.data.data
-    //     })
-    //   }
-    // })
+    wx.getStorage({
+      key:'access_token',
+      success:function(res1){
+        wx.request({
+          url:app.data.url+'/api/address/address_query',
+          method:'POST',
+          data:{access_token: res1.data,type:1,provincecode:e.currentTarget.id},
+          success:function(res){
+            console.log(res.data.data)
+            that.setData({
+              array2:res.data.data
+            })
+          }
+        })
+      }
+    })
   },
   getqu(e){
     console.log(e)
@@ -193,17 +203,22 @@ Page({
       area:'',
       array3:that.data.county1
     })
-    // wx.request({
-    //   url:app.globalData.url+'/api/member/address_query',
-    //   method:'POST',
-    //   data:{access_token: app.accessToken(),type:2,provincecode:that.data.provincecode,citycode:e.currentTarget.id},
-    //   success:function(res){
-    //     console.log(res.data.data)
-    //     that.setData({
-    //       array3:res.data.data
-    //     })
-    //   }
-    // })
+    wx.getStorage({
+      key:'access_token',
+      success:function(res1){
+        wx.request({
+          url:app.data.url+'/api/address/address_query',
+          method:'POST',
+          data:{access_token: res1.data,type:2,provincecode:that.data.provincecode,citycode:e.currentTarget.id},
+          success:function(res){
+            console.log(res.data.data)
+            that.setData({
+              array3:res.data.data
+            })
+          }
+        })
+      }
+    })
   },
   comfirm(e){
     this.setData({
